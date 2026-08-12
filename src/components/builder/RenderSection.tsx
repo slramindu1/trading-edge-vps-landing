6e816ce475ad9c4c84d9208164718e40438b1b72
slramindu1 <Ramindu.jiat@gmail.com>
2026-08-13 00:49:24 +0530

    chore: full checkup and fix production env configurations

--- src/components/builder/RenderSection.tsx
+++ src/components/builder/RenderSection.tsx
@@ -78,7 +78,7 @@ export async function RenderSection({ sectionKey }: { sectionKey: string }) {
                     key={index}
                     className="overflow-hidden rounded-2xl shadow-lg mb-4 break-inside-avoid group relative"
                   >
-                    <img
+                    <Image
                       src={src}
                       alt={`Image ${index + 1}`}
                       width={600}
